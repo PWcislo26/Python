47a9e85c8dcb7c853c065142213db7c4845ef8ac
PWcislo26 <piotr.wcislo@edu.uekat.pl>
2022-01-18 12:12:19 +0100

    updates

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pytesseract
 import cv2
+from cv2 import ADAPTIVE_THRESH_GAUSSIAN_C
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = cv2.imread('images/sample_text.jfif')
-img1 = cv2.imread('images/sample_text2.png')
-img2 = cv2.imread('images/sample_text3.png')
-img3 = cv2.imread('images/sample_text4.jpg')
+img = cv2.imread('images/captcha3.png')
+img1 = cv2.imread('images/captcha7.jpg')
+img2 = cv2.imread('images/noise.jpg')
+
 
 
 def read_from_image(img):
-    return pytesseract.image_to_string(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    filtered = cv2.adaptiveThreshold(cv2.GaussianBlur(gray, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    cv2.imshow('',filtered)
+    cv2.waitKey(0)
+    return pytesseract.image_to_string(filtered)
 
 
-print(read_from_image(img))
-print(read_from_image(img1))
+
 print(read_from_image(img2))
-print(read_from_image(img3))
